1c52eb59d2499c1d7582cca58a73eb6dda7b8e55
Kamil Trojnar <kamil.trojnar@gmail.com>
2019-12-07 07:33:01 +0000

    scheduler add

--- src/Components/Vetclinic/Client/ClientPage.tsx
+++ src/Components/Vetclinic/Client/ClientPage.tsx
@@ -1,10 +1,30 @@
+import "./ClientPage.css";
 import React, { Component, Fragment } from "react";
 import { AppointmentList, PetList } from "./ClientPage/index";
+import {
+  Inject,
+  ScheduleComponent,
+  Day,
+  Week,
+  WorkWeek,
+  Month
+} from "@syncfusion/ej2-react-schedule";
 
 export default class extends Component {
   render() {
     return (
       <Fragment>
+        <div className="container">
+          <div className="div1">Vet Clinic For Clients</div>
+          <div className="div2">list of vets</div>
+          <div className="div3">list of pets</div>
+          <div className="div4">
+            <ScheduleComponent>
+              <Inject services={[Day, Week, WorkWeek, Month]} />
+            </ScheduleComponent>
+          </div>
+        </div>
+
         <AppointmentList />
         <PetList />
       </Fragment>
